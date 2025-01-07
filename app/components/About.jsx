d468b40c6d8fb5cb139b0e48cab19e51eba5b54e
Khadijah <evesseacret@gmail.com>
2025-01-07 01:17:28 +0100

    Add main components for portfolio site: Header, About, Services, Work, Contact

---
+++ app/components/About.jsx
@@ -0,0 +1,65 @@
+import { assets, infoList, toolsData } from "@/assests/assets";
+import React from "react";
+import Image from "next/image";
+
+const About = () => {
+  return (
+    <div id="about" className="w-full px-[12%] py-10 scroll-mt-20">
+      <h4 className="text-center mb-2 text-lg font-Ovo">Introduction</h4>
+      <h2 className="text-center text-5xl font-Ovo">About Me</h2>
+
+      <div className="flex w-full flex-col lg:flex-row items-center gap-20 my-20">
+        <div className="w-64 sm:w-80 rounded 3xl max-w-none">
+          <Image
+            src={assets.user_image}
+            alt="user"
+            className="w-full rounded-3xl"
+          />
+        </div>
+        <div className="flex-1">
+          <p className="mb-10 max-w-2xl font-Ovo">
+            As a frontend engineer, I'm passionate about creating user
+            interfaces that are both visually appealing and easy to use. I excel
+            at developing responsive and accessible websites and applications,
+            ensuring a seamless and positive experience for users, across all
+            devices. I'm always eager to learn new technologies and techniques
+            to improve my skills and stay up-to-date with the latest trends in
+            web development. I'm excited to work with you to bring your ideas to
+            life and create a product that you and your users will love. Let's
+            build something amazing together!
+          </p>
+
+          <ul className="grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl">
+            {infoList.map(({ icon, iconDark, title, description }, index) => (
+              <li
+                className="border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black"
+                key={index}
+              >
+                <Image src={icon} alt="{title}" className="w-7 mt-3" />
+                <h3 className="my-4 font-semibold text-gray-700">
+                  {title}
+                  <p className="text-gray-600 text-sm">{description}</p>
+                </h3>
+              </li>
+            ))}
+          </ul>
+
+          <h4 className="my-6 text-gray-600 font-Ovo">Tools I use</h4>
+
+          <ul className="flex items-center gap-3 sm:gap-5">
+            {toolsData.map((tool, index) => (
+              <li
+                className="flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500"
+                key={index}
+              >
+                <Image src={tool} alt="Tool" className="w-5 sm:w-7" />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ app/components/Contact.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div id='contact' className="w-full px-[12%] py-10 scroll-mt-20">
+      <h4 className="text-center mb-2 text-lg font-Ovo">My Portfolio</h4>
+      <h2 className="text-center text-5xl font-Ovo">My latest work</h2>
+      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
+        Welcome to my digital playground. Here, you'll find a collection of
+        projects that showcase my passion for crafting engaging and
+        user-friendly experiences. Explore and discover how I bring ideas to
+        life through design and code.
+      </p>
+    </div>
+  )
+}
+
+export default Contact
