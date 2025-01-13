d40e8829e5cbc857b097d9cd0ab9b776f7888c6c
Khadijah <evesseacret@gmail.com>
2025-01-13 04:41:41 +0100

    Implement dark mode support across components and update styles accordingly

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,14 +2,14 @@ import { assets } from "@/assests/assets";
 import Image from "next/image";
 import React from "react";
 
-const Footer = () => {
+const Footer = ({isDarkMode}) => {
   return (
     <div className="mt-20">
       <div className="test-center">
-        <Image src={assets.logo} alt="" className="w-36 mx-auto mb-2" />
+        <Image src={isDarkMode ? assets.logo_dark : assets.logo} alt="" className="w-36 mx-auto mb-2" />
 
         <div className="w-max flex item-center gap-2 mx-auto">
-          <Image src={assets.mail_icon} alt="" className="w-6" />
+          <Image src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon} alt="" className="w-6" />
           k.kabir@devwithdijah.com
         </div>
       </div>
